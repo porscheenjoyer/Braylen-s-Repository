d3f13e9456f7b8fea44a99c903fc91aa65a7d07a
Period 6 <braylen.diec@talawanda.org>
2024-02-06 13:42:34 -0500

    142

---
+++ src/Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args){
+
+        int product;
+
+        product = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+
+        System.out.println(product);
+
+    }
+}
