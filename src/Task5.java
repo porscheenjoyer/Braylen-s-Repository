9a8baedebe4d764915d5320eafc8601284799fe4
Period 6 <braylen.diec@talawanda.org>
2024-02-06 14:04:39 -0500

    204

---
+++ src/Task5.java
@@ -0,0 +1,7 @@
+public class Task5 {
+    public static void main(String[] args){
+
+        System.out.println(" B****** \n BR***** \n BRA**** \n BRAY*** \n BRAYL** \n BRAYLE* \n BRAYLEN");
+
+    }
+}
