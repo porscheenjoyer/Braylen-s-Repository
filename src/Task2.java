9abf231e1b0b625a21c63d1a954d16cbb2011bff
Period 6 <braylen.diec@talawanda.org>
2024-02-06 13:39:43 -0500

    139

---
+++ src/Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+    public static void main(String[] args){
+
+        int sum;
+
+        sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+
+        System.out.println(sum);
+
+    }
+}
